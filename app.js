0cfb61f060cabe8420ffd0ebb120779884c1f640
Harvinder Singh <hs541156@gmail.com>
2024-03-13 17:48:14 +0530

    added HTML file with Parcel Bundler

--- app.js
+++
@@ -1,5 +0,0 @@
-// DOM Manipulation using React without JSX
-const heading = React.createElement('h2', {}, 'DOM Manipulation using React without JSX')
-const root = ReactDOM.createRoot(document.querySelector('#root'))
-
-root.render(heading)   
